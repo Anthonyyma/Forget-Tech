9395105b8960e7481ead02a45170f28ba911f469
Anthonyyma <56619066+Anthonyyma@users.noreply.github.com>
2021-08-12 10:56:54 -0600

    Update settings.py

--- getaway/settings.py
+++ getaway/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure--obsl@9&a9ax3lsc_)#6&t)waphiri&#7v0-e_r^g6lhm4ed7u'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
